a61953cb4c47e329bdfdbac9fb686366de814499
Marko Hakkarainen <hakkis68@gmail.com>
2024-05-16 15:30:27 +0300

    11b

--- cypress/e2e/pokedex_app.cy.js
+++ cypress/e2e/pokedex_app.cy.js
@@ -1,7 +1,16 @@
 describe("Pokedex", function () {
+  beforeEach(function () {
+    cy.visit("");
+  });
   it("front page can be opened", function () {
-    cy.visit("http://localhost:5000");
     cy.contains("ivysaur");
+    cy.contains(
+      "Pokémon and Pokémon character names are trademarks of Nintendo."
+    );
+  });
+  it("one can navigate from the main page to the page of a particular Pokemon", function () {
+    cy.contains("ivysaur").click();
+    cy.contains("chlorophyll");
     cy.contains(
       "Pokémon and Pokémon character names are trademarks of Nintendo."
     );
